59a03f2b9a10efc8b23f56dcf545a8503de4474b
Rob Ellis <rellis@slack-corp.com>
2017-05-11 22:17:55 -0700

    more cleanup

--- src/index.js
+++ src/index.js
@@ -1,104 +1,12 @@
 
-const _ = require("lodash");
-const debug = require("debug")("math");
-const safeEval = require("safe-eval");
-
-import conv from './convert';
-
-const mathExpressionSubs = {
-  'plus': '+',
-  'minus': '-',
-  'multiply': '*',
-  'multiplied': '*',
-  'x': '*',
-  'times': '*',
-  'divide': '/',
-  'divided': '/'
-};
-
-// Given an array for words it returns the evauated sum.
-// TODO - fractions
-// TODO, words should be the dict object with lem words to fix muliply / multipled etc
-const parse = function(words, prev){
-  debug("In parse with", words);
-  prev = prev || 0;
-  let expression = [];
-  let newexpression = [];
-  let word;
-  let square = false;
-
-  for (let i = 0; i < words.length; i++) {
-    // Convert words from word form to digit form.
-    let digit = conv.convertWordToNumber(words[i]);
-    if (digit !== undefined) {
-      words[i] = digit;
-    }
-
-    word = words[i];
-    // Convert word expression to evaluated equivilant.
-    if (mathExpressionSubs[word] !== undefined){
-      words[i] = mathExpressionSubs[word];
-    }
-  }
-
-  // Special case - square root
-  if (_.includes(words, "square") || _.includes(words, "squared")){
-    words = _.reject(words, function(w){ 
-      return ["squared","square","root","of"].indexOf(w) !== -1;
-    });
-    square = true;
-  }
-
-  for (let i = 0; i < words.length; i++){
-    word = words[i];
-    if (/[\d\*\+\-\/=%]|of|half|third|percent/.test(word)){
-      if (word == "half") {
-        newexpression.push(0.5);
-      } else if (word == "third"){
-       newexpression.push(.333333);
-      } else if (word == "of") {
-        expression.push("*");
-      } else if ((word == "%" || word == "percent" ) && isNumeric(words[i - 1])){
-        expression.pop();
-        expression.push(parseInt(words[i - 1]) / 100);
-      } else {
-        expression.push(word);
-      }
-    }
-  }
-
-  // Make numbers positive
-  for (let i = 0; i < expression.length; i++){
-    let curr = expression[i];
-    let next = expression[i + 1];
-    newexpression.push(curr);
-    if (/\d/.test(curr) && /\d/.test(next)){
-      newexpression.push("+");
-    }
-  }
-
-  try {
-    if (newexpression.length == 2 || newexpression[0] == "+"){
-      newexpression.unshift(prev);
-    }
-    debug("Eval", newexpression.join(" "));
-    let value = safeEval(newexpression.join(" "));
-
-    if (square) {
-      value = safeEval("Math.sqrt(" + value + ");");
-    }
-
-    return _.isFinite(value) ? +value.toFixed(2) : null;
-  } catch (e) {
-    debug("Error", e);
-    return null;
-  }
-};
-
-const isNumeric = function(num){
-  return !isNaN(num);
-};
+import conv from '../src/convert';
+import math from '../src/expression';
 
 export default{
-  parse
+  cardPlural: conv.cardPlural,
+  convertWordToNumber: conv.convertWordToNumber,
+  convertWordsToNumbers: conv.convertWordsToNumbers,
+  convertNumberToWord: conv.convertNumberToWord,
+  numberLookup: conv.numberLookup,
+  parse: math.parse
 };
--- test/convert.js
+++ test/convert.js
@@ -2,13 +2,10 @@
 
 import mocha from 'mocha';
 import should from 'should/as-function';
-
-
-import math from '../src/index';
 import conv from '../src/convert';
 
-
-
+import par from '../src/';
+console.log(par);
 
 describe('Convert', () => {
   it('convert word to number', (done) => {
@@ -51,68 +48,5 @@ describe('Convert', () => {
     should(conv.convertNumberToWord(111035)).eql('one hundred and eleven thousand and thirty-five');
     done();
   })
-
-});
-
-describe('Parse expression', () => {
-  it('Simple expression 1 + 1', (done) => {
-    const ans = math.parse("what is 1 + 1".split(" "));
-    should(ans).equal(2);
-    done();
-  });
-
-  it('Simple expression 2 (3 terms)', (done) => {
-    const ans = math.parse("What is 4 + 2 - 1".split(" "));
-    should(ans).equal(5);
-    done();
-  });
-
-  it('Word expression', (done) => {
-    const ans = math.parse("What is seven times eight".split(" "));
-    should(ans).equal(56);
-    done();
-  });
-
-  it('percent 1 expression', (done) => {
-    const ans = math.parse("What is 50 percent of 40".split(" "));
-    should(ans).equal(20);
-    done();
-  });
-
-  it('percent 2 expression', (done) => {
-    const ans = math.parse("What is half of six".split(" "));
-    should(ans).equal(3);
-    done();
-  });
-
-  it('div zero', (done) => {
-    const ans = math.parse("What is 7 divided by 0".split(" "));
-    should(ans).equal(null);
-    done();
-  });
-
-  it('fraction', (done) => {
-    const ans = math.parse("What is a third of 6".split(" "));
-    should(ans).equal(2);
-    done();
-  });
-
-  it('square root', (done) => {
-    const ans = math.parse("What is the square root of 9".split(" "));
-    should(ans).equal(3);
-    done();
-  });
-
-  it('square root 2', (done) => {
-    const ans = math.parse("What is the square root of 3 x 3 ".split(" "));
-    should(ans).equal(3);
-    done();
-  });
-
-  it('square root 3', (done) => {
-    const ans = math.parse("What is the 3 x 3 squared".split(" "));
-    should(ans).equal(3);
-    done();
-  });
 
 });
